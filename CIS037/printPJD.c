90f25e666f8ca6d835ee8ee4f5265d9177a65d61
tinotk <tinokhong@gmail.com>
2015-02-03 19:52:33 -0800

    Created by Coding Ground Client

---
+++ CIS037/printPJD.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printPJD()
+    {
+    puts("PPPPPPPPP");
+    puts("    P   P");
+    puts("    P   P");
+    puts("     P P\n");
+    puts("  JJ");
+    puts(" J");
+    puts("J");
+    puts(" J");
+    puts("  JJJJJJJ\n");
+    puts("DDDDDDDDD");
+    puts("D       D");
+    puts("D       D");
+    puts(" D     D");
+    puts("  DDDDD");
+    }
+    
