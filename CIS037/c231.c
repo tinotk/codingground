52ce92aff7b406149593cc4ee147716099b00774
tinotk <tinokhong@gmail.com>
2015-02-03 21:39:08 -0800

    Created by Coding Ground Client

---
+++ CIS037/c231.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void c231()
+{
+puts("PPPPPPPPP");
+puts("    P   P");
+puts("    P   P");
+puts("     P P\n");
+puts("  JJ");
+puts(" J");
+puts("J");
+puts(" J");
+puts("  JJJJJJJ\n");
+puts(" DDDDDDDDD");
+puts("D D");
+puts("D D");
+puts(" D D");
+puts(" DDDDD");
+}
